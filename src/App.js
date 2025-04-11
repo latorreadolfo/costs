5045a59db985c65a88809e4d2b5746aa31db1adb
latorreadolfo <adolfo3eng@gmail.com>
2025-04-11 14:02:06 -0300

    design components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import Project from "./components/pages/Project";
+import NewProject from "./components/pages/NewProject";
+
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Container customClass="minHeight">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Project />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+
+      <Footer />
+    </Router>
   );
 }
 
